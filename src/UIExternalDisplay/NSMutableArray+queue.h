b82dd90ed8cb219fff7dea913e2d5f723a119605
Scott Beardsley <sbeardsley@codelinq.com>
2010-08-17 17:16:11 -0400

    Create Queue Class

---
+++ src/UIExternalDisplay/NSMutableArray+queue.h
@@ -0,0 +1,16 @@
+//
+//  NSMutableArray+queue.h
+//  UIExternalDisplay
+//
+//  Created by Scott Beardsley on 8/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSMutableArray (queue) {
+
+}
+
+@end
---
+++ src/UIExternalDisplay/NSMutableArray+queue.m
@@ -0,0 +1,14 @@
+//
+//  NSMutableArray+queue.m
+//  UIExternalDisplay
+//
+//  Created by Scott Beardsley on 8/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSMutableArray+queue.h"
+
+
+@implementation NSMutableArray (queue)
+
+@end
